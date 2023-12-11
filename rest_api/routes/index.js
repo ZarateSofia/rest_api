fc3245c670209a8662a5686601e57506b4d4891c
ZarateSofia <118627198+ZarateSofia@users.noreply.github.com>
2023-12-11 10:31:36 -0500

    Parte final

--- rest_api/routes/index.js
+++ rest_api/routes/index.js
@@ -6,4 +6,86 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/save', function(req, res, next) { 
+  let {SupplierName, ContactName, Address, City, PostalCode, Country, Phone} = req.body;
+        
+    Suppliers.create({
+        SupplierName: SupplierName, 
+        ContactName: ContactName, 
+        Address: Address, 
+        City: City, 
+        PostalCode: PostalCode, 
+        Country: Country, 
+        Phone: Phone
+    })
+    .then(data => {  
+      res.json(data);  
+  })  
+  .catch(error => res.status(400).send(error)) 
+});
+
+router.get('/findById/:id', function(req, res, next) {
+
+  let id = parseInt(req.params.id);
+
+  Suppliers.findOne({  
+      where: { 
+        [Op.and]: [
+          {SupplierID: id}
+        ]
+      }
+  })  
+  .then(data => {  
+      res.json(data);  
+  })  
+  .catch(error => res.status(400).send(error)) 
+});
+
+router.get('/findAll/json', function(req, res, next) {
+  Users.findAll({  
+  })  
+  .then(users => {  
+      res.json(users);  
+  })  
+  .catch(error => res.status(400).send(error)) 
+});
+
+router.put('/update', function(req, res, next) {  
+
+  let {id, email, username, password} = req.body;
+      
+  Users.update({
+    email: email,
+    username: username,
+    password: password,
+    logins: 0,
+    last_login: 0
+  },
+  {
+      where: {
+        id: parseInt(id)
+      }
+  })
+  .then(users => {  
+    res.json(users);  
+})  
+.catch(error => res.status(400).send(error)) 
+});
+
+router.delete('/delete/:id', function(req, res, next) {  
+
+  let id = parseInt(req.params.id);
+      
+  Users.destroy({
+    where: { 
+      id: id
+    }
+  })
+  .then(users => {  
+  res.json(users);  
+})  
+.catch(error => res.status(400).send(error)) 
+});
+
+
 module.exports = router;
