4be9235e8cc28b4b8542c2d6a0ddff268ee8d015
Carlos <sdfsdk@gmail.com>
2023-12-13 10:03:31 -0500

    Implementación de los verbos HTTP

--- rest_api/routes/suppliers.js
+++ rest_api/routes/suppliers.js
@@ -4,20 +4,88 @@ var router = express.Router();
 const { Sequelize, Op } = require('sequelize');
 const Suppliers = require('../models').suppliers;
 
-router.get('/findAll', function(req, res, next) {
-    res.send("GET All")
-});
 router.get('/findById/:id', function(req, res, next) {
-    res.send("GET By Id")
-});
-router.post('/save', function(req, res, next) { 
-    res.send("POST")
-});
-router.put('/update/:id', function(req, res, next) { 
-    res.send("PUT")
-});  
-router.delete('/delete/:id', function(req, res, next) { 
-    res.send("DELETE")
-});
+
+    let id = parseInt(req.params.id);
+  
+    Suppliers.findOne({  
+        where: { 
+          [Op.and]: [
+            {SupplierID: id}
+          ]
+        }
+    })  
+    .then(data => {  
+        res.json(data);  
+    })  
+    .catch(error => res.status(400).send(error)) 
+  });
+
+  router.post('/save', function(req, res, next) { 
+    let {SupplierName, ContactName, Address, City, PostalCode, Country, Phone} = req.body;
+          
+      Suppliers.create({
+          SupplierName: SupplierName, 
+          ContactName: ContactName, 
+          Address: Address, 
+          City: City, 
+          PostalCode: PostalCode, 
+          Country: Country, 
+          Phone: Phone
+      })
+      .then(data => {  
+        res.json(data);  
+    })  
+    .catch(error => res.status(400).send(error)) 
+  });
+
+  router.get('/findAll/json', function(req, res, next) {
+    Suppliers.findAll({  
+    })  
+    .then(data => {  
+        res.json(data);  
+    })  
+    .catch(error => res.status(400).send(error)) 
+  });
+  
+  router.put('/update', function(req, res, next) {  
+
+    let {SupplierID,SupplierName, ContactName, Address, City, PostalCode, Country, Phone} = req.body;
+        
+    Suppliers.update({
+        SupplierName: SupplierName, 
+        ContactName: ContactName, 
+        Address: Address, 
+        City: City, 
+        PostalCode: PostalCode, 
+        Country: Country, 
+        Phone: Phone
+    },
+    {
+        where: {
+          SupplierID: parseInt(SupplierID)
+        }
+    })
+    .then(users => {  
+      res.json(users);  
+  })  
+  .catch(error => res.status(400).send(error)) 
+  });
+  
+  router.delete('/delete/:id', function(req, res, next) {  
+
+    let id = parseInt(req.params.id);
+        
+    Suppliers.destroy({
+      where: { 
+        SupplierID: id
+      }
+    })
+    .then(users => {  
+    res.json(users);  
+  })  
+  .catch(error => res.status(400).send(error)) 
+  });
+  
 
 module.exports = router;
